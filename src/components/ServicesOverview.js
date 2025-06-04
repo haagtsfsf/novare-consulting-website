6b424bc424f0a12366d7691cca9b0e878cc1ba6c
haagtsfsf <customretfin@proton.me>
2025-06-04 20:00:59 +0200

    Update ServicesOverview.js

--- src/components/ServicesOverview.js
+++ src/components/ServicesOverview.js
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { 
   Building2, 
@@ -143,3 +143,4 @@ const ServicesOverview = () => {
     </section>
   );
 };
+export default ServicesOverview;
